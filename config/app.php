b20d912653479c3df0b65717d8be13ddcfa805d7
nick <nicholasjkitchen@gmail.com>
2020-10-27 06:40:57 +1100

    added Bugsnag

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\DomPDF\ServiceProvider::class,
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
 
         /*
          * Application Service Providers...
