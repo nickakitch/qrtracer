09dee3ecc4e875c219b6e6db4b257ebea6ecdc4b
nick <nicholasjkitchen@gmail.com>
2020-10-31 16:33:02 +1100

    added info about encryption to the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,16 @@
 @extends('layout.app', ['title' => 'QR Tracer - Free Service To Record Your Visitors For Contact Tracing Purposes'])
 
 @section('content')
-    <section class="jumbotron">
+    <section class="jumbotron pb-3 pt-4">
         <h1>Set up contact tracing for your business.</h1>
         <p class="lead">This free and simple to use tool will allow you to easily record every visitor to your business for COVID-19 contact tracing purposes.</p>
-        <a class="btn btn-primary btn-lg" href="{{ route('register') }}" role="button">Get Started</a>
+        <p>
+            <a class="btn btn-primary btn-lg" href="{{ route('register') }}" role="button">Get Started</a>
+        </p>
+        <p class="small text-muted">
+            <i class="fas fa-lock mr-1"></i>
+            Information collected (name, phone, and email) is securely encrypted within the database.
+        </p>
     </section>
 @endsection
 
